f800ab95a125d43ae24beda5e3c6bc42d9dfe020
DavidTG7 <davidthekoa@gmail.com>
2022-03-20 21:02:07 -0500

    NodeJS practice using NPM

---
+++ node_js/intro/index.js
@@ -0,0 +1,10 @@
+var superheroes = require("superheroes");
+
+var mySuperHeroName = superheroes.random();
+console.log(mySuperHeroName);
+
+
+var supervillains = require("supervillains");
+
+var mySuperVillain = supervillains.random();
+console.log(mySuperVillain);
