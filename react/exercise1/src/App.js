8944a1a9a5677abbe6bca07374d76dee60d91181
DavidTG7 <davidthekoa@gmail.com>
2022-06-13 16:15:41 -0500

    App js file for react exercise

---
+++ react/exercise1/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Header } from './Header/Header';
+
+
+function App() {
+  return(
+    <>
+      <Header src='https://blog.hubspot.es/hs-fs/hubfs/ES%20Blog%20images/Los%2015%20logos%20m%C3%A1s%20creativos%20e%20inspiradores%20del%20mundo/logo_creativo_barbie.png?width=800&name=logo_creativo_barbie.png' text='HomeCenter' /> 
+      <Header src='https://blog.hubspot.es/hs-fs/hubfs/ES%20Blog%20images/Los%2015%20logos%20m%C3%A1s%20creativos%20e%20inspiradores%20del%20mundo/logo_famoso_starbucks.jpg?width=650&name=logo_famoso_starbucks.jpg' text='Exito' /> 
+      <Header src='https://blog.hubspot.es/hs-fs/hubfs/ES%20Blog%20images/Los%2015%20logos%20m%C3%A1s%20creativos%20e%20inspiradores%20del%20mundo/ejemplo_logo_famoso_gap.png?width=650&name=ejemplo_logo_famoso_gap.png' text='Alkosto' /> 
+
+    </>
+  );
+}
+
+export default App;
